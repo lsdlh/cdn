cce9b83b910c92fdc99564fc02c2b2e3931b0135
lsdlh <1763221541@qq.com>
2019-09-11 16:19:13 +0800

    第五次

--- live2d/assets/autoload.js
+++ live2d/assets/autoload.js
@@ -1,18 +1,18 @@
 // 加载 CSS
-$("<link>").attr({href: "https://lsdlh.club/live2d/assets/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+$("<link>").attr({href: "https://cdn.jsdelivr.net/gh/lsdlh/cdn@1.2/live2d/assets/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
 
 // 插入 DIV
 $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
 
 // 加载 JS
 $.ajax({
-	url: 'https://lsdlh.club/live2d/assets/waifu-tips.js',
+	url: 'https://cdn.jsdelivr.net/gh/lsdlh/cdn@1.2/live2d/assets/waifu-tips.js',
 	dataType:"script",
 	cache: true,
 	async: false
 });
 $.ajax({
-	url: 'https://lsdlh.club/live2d/assets/live2d.js',
+	url: 'https://cdn.jsdelivr.net/gh/lsdlh/cdn@1.2/live2d/assets/live2d.js',
 	dataType:"script",
 	cache: true,
 	async: false
@@ -24,4 +24,4 @@ $.ajax({
 live2d_settings['modelId'] = 1;                  // 默认模型 ID
 live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
 /* 在 initModel 前添加 */
-initModel('https://lsdlh.club/live2d/assets/waifu-tips.json');
+initModel('https://cdn.jsdelivr.net/gh/lsdlh/cdn@1.2/live2d/assets/waifu-tips.json');
